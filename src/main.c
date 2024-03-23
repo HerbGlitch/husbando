839bce5423fc92fd8519e372a76d40538823e388
herbglitch <bighebe@gmail.com>
2024-03-23 03:06:22 -0600

    testing console

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
+#include "arc/console/buffer.h"
+#include "arc/std/bool.h"
 #include <ncurses.h>
 #include <arc/console/view.h>
+#include <arc/console/shell.h>
 #include <arc/console/element.h>
 #include <arc/math/point.h>
+#include <arc/math/rectangle.h>
 #include <arc/std/string.h>
 //#include "arc/ssh/ssh.h"
 
@@ -24,86 +28,49 @@ alias pdesktop4='ydotool key 125:1 5:1 5:0 125:0'
 alias pclose='ydotool key 125:1 46:1 46:0 125:0'
 */
 
-int main(int argc, char *argv[]){
-    // ARC_Ssh_Create(&ssh);
-    ARC_ConsoleView *mainConsoleView;
-    ARC_ConsoleView_Create(&mainConsoleView, NULL);
-    ARC_ConsoleView_SetBorder(mainConsoleView, ARC_CONSOLE_VIEW_BORDER_DEFAULT);
+void Temp_ConsoleShell_UpdateFn(ARC_ConsoleShell *shell){
+    ARC_String *prompt;
+    ARC_String_CreateWithStrlen(&prompt, "SHELL > ");
 
-    ARC_String *string0;
-    ARC_String *string1;
-    ARC_String *string2;
-    ARC_String *string3;
+    ARC_Point currentPos = (ARC_Point){ 1, 1 };
 
-    ARC_String_CreateWithStrlen(&string0, "OPTION 1");
-    ARC_String_CreateWithStrlen(&string1, "OPTION 2");
-    ARC_String_CreateWithStrlen(&string2, "OPTION 3");
-    ARC_String_CreateWithStrlen(&string3, "OPTION 4");
+    ARC_Bool running = ARC_True;
+    while(running){
+        ARC_ConsoleView_RenderStringAt(shell->view, prompt, currentPos);
 
-    ARC_ConsoleElement *element0;
-    ARC_ConsoleElement *element1;
-    ARC_ConsoleElement *element2;
-    ARC_ConsoleElement *element3;
-
-    ARC_ConsoleElement_Create(&element0, 0, ARC_CONSOLE_ELEMENT_FLAG_NONE, string0, (ARC_Point){1, 1}, ARC_ConsoleElement_DefaultRenderFn);
-    ARC_ConsoleElement_Create(&element1, 0, ARC_CONSOLE_ELEMENT_FLAG_NONE, string1, (ARC_Point){1, 2}, ARC_ConsoleElement_DefaultRenderFn);
-    ARC_ConsoleElement_Create(&element2, 0, ARC_CONSOLE_ELEMENT_FLAG_NONE, string2, (ARC_Point){1, 3}, ARC_ConsoleElement_DefaultRenderFn);
-    ARC_ConsoleElement_Create(&element3, 0, ARC_CONSOLE_ELEMENT_FLAG_NONE, string3, (ARC_Point){1, 4}, ARC_ConsoleElement_DefaultRenderFn);
-
-    ARC_ConsoleView_AddElement(mainConsoleView, element0);
-    ARC_ConsoleView_AddElement(mainConsoleView, element1);
-    ARC_ConsoleView_AddElement(mainConsoleView, element2);
-    ARC_ConsoleView_AddElement(mainConsoleView, element3);
-
-    ARC_ConsoleElement *selectedElement = ARC_ConsoleView_GetElement(mainConsoleView, 0);
-    ARC_ConsoleElement_SetSelected(selectedElement, ARC_True);
-
-    ARC_ConsoleView_RenderElements(mainConsoleView);
-
-    int32_t character = getch();
-    int32_t selected = 0;
-    while(character != KEY_F(1)){
-        ARC_ConsoleElement_SetSelected(selectedElement, ARC_False);
-
-        switch(character){
-            case KEY_UP:
-                if(selected == 0){
-                    break;
-                }
-                selected--;
-                break;
-
-            case KEY_DOWN:
-                if(selected == 3){
-                    break;
-                }
-                selected++;
-                break;
+        ARC_String *input = ARC_ConsoleView_GetStringInput(shell->view, (ARC_Point){ currentPos.x + prompt->length, currentPos.y });
+        currentPos.y++;
+        if(input == NULL){
+            continue;
         }
 
-        selectedElement = ARC_ConsoleView_GetElement(mainConsoleView, selected);
-        ARC_ConsoleElement_SetSelected(selectedElement, ARC_True);
+        if(ARC_String_SubstringEqualsCString(input, 0, "exit", 4)){
+            running = ARC_False;
+        }
 
-        ARC_ConsoleView_RenderElements(mainConsoleView);
-        character = getch();
+        ARC_ConsoleBuffer_AddString(shell->buffer, input);
+        ARC_String_Destroy(input);
+        ARC_ConsoleBuffer_Render(shell->buffer, shell->view);
     }
 
-    ARC_ConsoleView_RemoveElement(mainConsoleView, 0);
-    ARC_ConsoleView_RemoveElement(mainConsoleView, 0);
-    ARC_ConsoleView_RemoveElement(mainConsoleView, 0);
-    ARC_ConsoleView_RemoveElement(mainConsoleView, 0);
+    ARC_String_Destroy(prompt);
+}
 
-    ARC_String_Destroy(element0->string);
-    ARC_String_Destroy(element1->string);
-    ARC_String_Destroy(element2->string);
-    ARC_String_Destroy(element3->string);
+int main(int argc, char *argv[]){
+    // ARC_Ssh_Create(&ssh);
+    ARC_ConsoleView *view;
+    ARC_ConsoleView_Create(&view, (ARC_Rect){ 0, 0, 0, 0 });
+    //ARC_ConsoleView_SetBorder(view, ARC_CONSOLE_VIEW_BORDER_DEFAULT);
 
-    ARC_ConsoleElement_Destroy(element0);
-    ARC_ConsoleElement_Destroy(element1);
-    ARC_ConsoleElement_Destroy(element2);
-    ARC_ConsoleElement_Destroy(element3);
+    ARC_ConsoleShell *shell;
+    ARC_ConsoleShell_Create(&shell, view, Temp_ConsoleShell_UpdateFn);
 
-    ARC_ConsoleView_Destroy(mainConsoleView);
+    ARC_ConsoleShell_Render(shell);
+
+    ARC_ConsoleShell_Update(shell);
+
+    ARC_ConsoleShell_Destroy(shell);
+    ARC_ConsoleView_Destroy(view);
 
     // ARC_Ssh_Destroy(ssh);
     return 0;
